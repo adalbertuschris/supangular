457a43ed0bc5ebb0e2512967ad329f985d6128dc
Wojciech Krzysztofowicz <wojciech.krzysztofowicz@gmail.com>
2023-09-13 12:49:08 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'veltripo_client';
+}
