7ab7f1789710ecefdc7f8d959ff2b816922c054d
Wojciech Krzysztofowicz <wojciech.krzysztofowicz@gmail.com>
2023-09-19 23:13:59 +0200

    Add ngrx store, effects, dev-tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiClientsModule } from './api-clients/api-clients.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, ApiClientsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ApiClientsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
